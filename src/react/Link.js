dc8d32bb7e577fe96056715436c53beac43662a8
Michal Kvasničák <michal.kvasnicak@gmail.com>
2015-12-05 17:42:51 +0100

    fix link blinking in case that routex is in transitioning state

--- src/react/Link.js
+++ src/react/Link.js
@@ -10,8 +10,8 @@ export default function createLink(React, connect) {
             query: PropTypes.object,
             children: PropTypes.oneOfType([PropTypes.element, PropTypes.array]),
             stateProps: PropTypes.shape({
-                active: PropTypes.object.isRequired,
-                inactive: PropTypes.object.isRequired
+                active: PropTypes.object,
+                inactive: PropTypes.object
             }),
             router: PropTypes.object.isRequired
         };
@@ -23,32 +23,35 @@ export default function createLink(React, connect) {
             }).isRequired
         };
 
+        constructor() {
+            super();
+
+            this.state = {
+                active: false
+            };
+
+            //this.isActive = this.isActive.bind(this);
+        }
+
+        componentWillMount() {
+            this.isActive();
+        }
+
+        componentWillReceiveProps() {
+            this.isActive();
+        }
+
         render() {
-            const { to, query, router } = this.props;
-            let { stateProps, ...props } = this.props;
+            const { to, query } = this.props;
+            let { stateProps: stateProps = {}, ...props } = this.props;
             const href = this.context.store.router.createHref(to, query);
-            const { state, route } = router;
 
-            if (state === 'TRANSITIONED' && stateProps && route) {
-                let matches = href === route.pathname;
+            stateProps = stateProps[this.state.active ? 'active' : 'inactive'] || {};
 
-                if (!matches) {
-                    if (href === '/') {
-                        matches = true;
-                    } else if (href.length < route.pathname.length) {
-                        matches = (new RegExp(`^(${href}|${href}/.*)$`)).test(route.pathname);
-                    }
-                }
-
-                stateProps = stateProps[
-                    matches ? 'active' : 'inactive'
-                ];
-
-                props = {
-                    ...props,
-                    ...stateProps
-                };
-            }
+            props = {
+                ...props,
+                ...stateProps
+            };
 
             return (
                 <a
@@ -58,6 +61,26 @@ export default function createLink(React, connect) {
                     {this.props.children}
                 </a>
             );
+        }
+
+        isActive() {
+            const { router, stateProps, to, query } = this.props;
+            const { state, route } = router;
+            const href = this.context.store.router.createHref(to, query);
+
+            if (state === 'TRANSITIONED' && stateProps && route) {
+                const matches = href === route.pathname;
+
+                if (!matches) {
+                    if (href === '/') {
+                        this.setState({ active: true });
+                    } else if (href.length < route.pathname.length) {
+                        this.setState({ active: (new RegExp(`^(${href}|${href}/.*)$`)).test(route.pathname) });
+                    }
+                } else {
+                    this.setState({ active: true });
+                }
+            }
         }
 
         handleClick(e) {
